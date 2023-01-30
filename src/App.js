2442464750c23e86be3ac6723ef80609f8c41530
Jumoke Oyinloye <oyinloye.jumoke12@gmail.com>
2023-01-30 16:59:26 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import axios from "axios";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Download } from "./components/Download.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import MovieList from "./components/MovieList";
-
 const App = () => {
   return (
     <div className="movie-container">
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,37 +4,46 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Download from "./Download";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
-
-// import { Link } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
 
 const MovieList = (props) => {
   const [search, setSearch] = useState("");
   const [movies, setMovies] = useState([]);
   const [downloads, setDownloads] = useState([]);
+  const [alert, setAlert] = useState(false);
+  const [spinner, setSpinner] = useState(false);
 
-  const searchMovies = (e) => {
-    {
+  function searchMovies() {
+    try {
+      setSpinner(true);
+      console.log(spinner);
       axios
         .get("http://www.omdbapi.com/?s=" + search + "&apikey=5a7c6087")
-        .then((res) => setMovies(res.data.Search))
+        .then((res) => setMovies(res.data.Search), setSpinner(false))
         .catch((err) => console.log(err));
+    } catch (error) {
+      console.log(error);
     }
-  };
-
+  }
   function downloadMovie(movie) {
-    console.log(movie);
+    setAlert(true);
     downloads.push(movie);
+    setTimeout(function () {
+      setAlert(false);
+    }, 2000);
     localStorage.setItem("movies", JSON.stringify(downloads));
   }
 
   return (
-    <Container>
-      <Navbar className="nav-bar">
-        <Container>
+    <Container className="Container">
+      <Navbar>
+        <Container className="nav-bar">
           <Navbar.Brand style={{ color: "white" }}>
             <h1>Movie App</h1>
 
@@ -44,7 +53,15 @@ const MovieList = (props) => {
               </Routes>
             </BrowserRouter> */}
           </Navbar.Brand>
-
+          {alert && (
+            <div className="Alert">
+              {["success"].map((variant) => (
+                <Alert key={variant} variant={variant}>
+                  <Alert.Link href="#">Downloaded</Alert.Link>
+                </Alert>
+              ))}
+            </div>
+          )}
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -62,47 +79,67 @@ const MovieList = (props) => {
           </Form>
         </Container>
       </Navbar>
+      <Link to="/download" element={<Download />}>
+        Downloads
+      </Link>
+
       <Row>
+        {/* {spinner && (
+          <div className="spinner">
+            {" "}
+            <Spinner variant="success" />
+          </div>
+        )} */}
         {movies.map((movie, index) => (
           <Col className="pt-2" key={index}>
             <div className="border-success movie-box">
-              <img src={movie.Poster} className="movie-img" alt="movie"></img>
-              <div className="movie-details">
-                {" "}
+              <img src={movie.Poster} className="movie-img" alt="movie"></img>{" "}
+              <div className="movie-info">
                 <h1
                   style={{
+                    id: "text1",
+                    fontWeight: "800",
                     color: "white",
-                    fontSize: "12px",
+                    fontSize: "14px",
                     marginTop: "10px",
-                    overflowWrap: "break-word",
                   }}
                 >
-                  Title: {movie.Title}
+                  Title : {movie.Title}
                 </h1>
-                <div className="movie-info">
-                  <h1
-                    style={{
-                      color: "white",
-                      fontSize: "12px",
-                      marginTop: "10px",
-                    }}
+                <h1
+                  style={{
+                    fontWeight: "800",
+                    id: "text1",
+                    color: "white",
+                    fontSize: "14px",
+                    marginTop: "10px",
+                  }}
+                >
+                  {" "}
+                  Year : {movie.Year}
+                </h1>
+
+                <h1
+                  style={{
+                    fontWeight: "800",
+                    id: "text1",
+                    color: "white",
+                    fontSize: "14px",
+                    marginTop: "10px",
+                  }}
+                >
+                  {" "}
+                  Type : {movie.Type}
+                </h1>
+                <div>
+                  <button
+                    onClick={() => downloadMovie(movie)}
+                    className="download-btn"
                   >
-                    {" "}
-                    Year: {movie.Year}
-                  </h1>
-                  <h1
-                    style={{
-                      color: "white",
-                      fontSize: "12px",
-                      marginTop: "10px",
-                    }}
-                  >
-                    {" "}
-                    Type: {movie.Type}
-                  </h1>
-                  <button onClick={() => downloadMovie(movie)}>Download</button>
+                    Download
+                  </button>
                 </div>
-              </div>
+              </div>{" "}
             </div>
           </Col>
         ))}
